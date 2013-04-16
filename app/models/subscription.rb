5c766d9304fe508c24d9758ea75d79ffb5a51b73
Roman Gurovich <romangurovich@gmail.com>
2013-04-16 01:02:59 -0700

    Add user sign up and edit pages w/ subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ActiveRecord::Base
   attr_accessible :subscription_plan_id, :user_id
-  validates :user_id, uniqueness: { scope: :subscription_plan_id }
+  #validates :user_id, uniqueness: { scope: :subscription_plan_id }
 
   belongs_to :subscription_plan
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :password, :remember_token
+  attr_accessible :name, :password, :remember_token, :subscription_plan_ids
   validates :name, :password, presence: true, uniqueness: true
 
   has_many :subscriptions
